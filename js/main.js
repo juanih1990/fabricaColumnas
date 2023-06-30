955455ab116aded2feae2e413c727ab49962f820
juan Hernandez <juanihernandez1990@gmail.com>
2023-06-30 20:24:53 -0300

    btn enviar

--- js/main.js
+++ js/main.js
@@ -69,6 +69,7 @@ enviarPresupuesto.addEventListener('click',e =>{
         enviarPresupuesto.classList.replace("round","hidden")
     }
     else{
+        vaciar()
         e.target.classList.toggle("round")
         e.target.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
@@ -82,8 +83,7 @@ enviarPresupuesto.addEventListener('click',e =>{
         for (const resetCant of productos) {
                 resetCant.carrito = 0
         }
-            pintar(productos)  
-            vaciar()
+            pintar(productos)    
     }
 })
 filtrarColumnas.addEventListener('click', e =>{
